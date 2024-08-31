e0deb52d1a72a60e3ea8c43b83b646fafd6db578
ManiSai <63541439+ManiSai18@users.noreply.github.com>
2024-08-31 11:21:42 +0530

    first commit

--- btre/settings.py
+++ btre/settings.py
@@ -34,12 +34,17 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    "listings.apps.ListingsConfig",
+    "realtors.apps.RealtorsConfig",
+    "accounts.apps.AccountsConfig",
+    "contacts.apps.ContactsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +83,11 @@ WSGI_APPLICATION = "btre.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'btredb',
+        'USER':'postgres',
+        'PASSWORD':'ramukjar',
+        'HOST':'localhost'
     }
 }
 
@@ -117,13 +125,34 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_URL = "static/"
-STATICFILES_DIR=[
-    os.path.join(BASE_DIR,'btre/static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static'),
 ]
+#MEDIA FOLDER SETTINGS
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#MESSAGES 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.ERROR:'danger'
+}
+
+#Email Config
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='rajkumarr712002@gmail.com'
+EMAIL_HOST_PASSOWORD='alwyasRajkumar@710!'
+EMAIL_USE_TLS = True
